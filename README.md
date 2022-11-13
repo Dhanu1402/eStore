4d8f99b2fdc7f3acb5dacb663e0101bfd2a76ab8
Dhanu1402 <dhanukumar2000@gmail.com>
2022-11-13 19:11:20 +0530

    Fetch Products from Backend

--- README.md
+++ README.md
@@ -19,6 +19,7 @@
    3. create router for product screen
 
 5. Create Node.Js Server
+
    1. run NPM init in root folder
    2. Update package.json set type: module
    3. add .js to imports
@@ -31,3 +32,10 @@
    10. create route for /api/products
    11. return products
    12. run npm start
+
+6. Fetch Products from Backend
+   1. set proxy in package.json
+   2. npm install axios
+   3. use state hook
+   4. use effect hook
+   5. use reducer hook
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,12 +1,26 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import data from '../data';
+import axios from 'axios';
+// import data from '../data';
 
 function HomeScreen() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      // sending ajax request
+      const result = await axios.get('/api/products');
+      setProducts(result.data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div>
       <h1>Featured Products</h1>
       <div className="products">
-        {data.products.map((product) => (
+        {/* remove data.products and use products as now data will be coming from backend and not from frontend */}
+        {products.map((product) => (
           <div className="product" key={product.slug}>
             {/* key is required for React to know which element to update and it is unique */}
             <Link to={`/product/${product.slug}`}>
